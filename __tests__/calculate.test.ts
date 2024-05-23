c28bfd1ef89035cf6c81bbea18a95b73b0a5c252
shuheitachikawa <shuhei.tusx@gmail.com>
2024-05-23 22:44:20 +0900

    test

--- __tests__/calculate.test.ts
+++ __tests__/calculate.test.ts
@@ -1,8 +1,34 @@
-import React, { ReactElement } from 'react';
-import { render, screen, fireEvent } from "@testing-library/react";
-import 
-describe("calculate", () => {
-  it("返す値のテスト", async () => {
-      
-  });
-});
+import { calculate } from '@/app/api/dashboard/nextSleepTime/_utils/calculate'
+
+describe('calculate', () => {
+  describe('引数Aがhogeのとき', () => {
+    const a = 'hoge'
+    describe('引数Bがfugaのとき', () => {
+      const b = 'fuga'
+      it('返す値のテスト', async () => {
+        calculate(a, b)
+      })
+    })
+    describe('引数Bがhogeのとき', () => {
+      const b = 'hoge'
+      it('返す値のテスト', async () => {
+        calculate(a, b)
+      })
+    })
+  })
+  describe('引数Aがhugaのとき', () => {
+    const a = 'huga'
+    describe('引数Bがfugaのとき', () => {
+      const b = 'fuga'
+      it('返す値のテスト', async () => {
+        calculate(a, b)
+      })
+    })
+    describe('引数Bがhogeのとき', () => {
+      const b = 'hoge'
+      it('返す値のテスト', async () => {
+        calculate(a, b)
+      })
+    })
+  })
+})
