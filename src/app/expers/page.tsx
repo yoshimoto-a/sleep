5b295c3075e73c582f248881985e44c1deed6ac0
rs-akane <rs.akane.y@gmail.com>
2024-06-04 10:18:41 +0900

    講座実績追記

--- src/app/expers/page.tsx
+++ src/app/expers/page.tsx
@@ -14,33 +14,26 @@ export default function Home() {
         <h2 className="py-5 text-xl">監修した専門家</h2>
       </div>
 
-      <div className="flex items-center">
-        <div className="text-center w-3/5">
-          <h3>
-            <span className="text-sm">imaneru 代表</span>
-            <br />
-            坪根理恵先生
-          </h3>
-          <div className="text-sm mt-3">
-            2歳半差の2児の母
-            <br />
-            国際認定資格 IPHI
-            <br />
-            妊婦と乳幼児の
-            <br />
-            睡眠コンサルタント
-          </div>
-        </div>
-        <div className="m-5 w-2/5">
-          <Image
-            alt="tsuboneRie"
-            src={"/_topPage/rie.jpg"}
-            width={150}
-            height={0}
-            layout="intrinsic"
-          />
-        </div>
+      <div className="flex justify-center">
+        <Image
+          alt="tsuboneRie"
+          src={"/_topPage/rie.jpg"}
+          width={200}
+          height={0}
+          layout="intrinsic"
+        />
       </div>
+      <h3 className="text-center pt-5">
+        <span className="text-sm">imaneru 代表</span>
+        <br />
+        坪根理恵先生
+      </h3>
+      <div className="text-sm mt-3 flex justify-center">
+        2歳半差の2児の母
+        <br />
+        国際認定資格 IPHI妊婦と乳幼児の睡眠コンサルタント
+      </div>
+
       <div className="m-3">
         <p>
           国際認定資格
@@ -61,6 +54,20 @@ export default function Home() {
           など、乳幼児の睡眠についてお悩みがある場合は、ぜひ一度相談してみてください。
         </p>
       </div>
+
+      <h3 className="text-center pt-5 font-medium">講座実績</h3>
+      <ul className="mt-3">
+        <li className="text-sm">
+          八王子市親子つどいの広場ゆめきっず「赤ちゃんの睡眠・寝かしつけ講座」
+        </li>
+        <li className="text-sm">
+          八王子市親子ふれあい広場ぽかぽかひろば
+          「赤ちゃんの睡眠・寝かしつけ講座」
+        </li>
+        <li className="text-sm">
+          八南助産師会 助産師様向け「乳幼児の睡眠講座」
+        </li>
+      </ul>
       <div className="mt-8 flex justify-center">
         <div className="px-5 py-2 w-40 border border-gray-400	 rounded-full text-center">
           <Link
