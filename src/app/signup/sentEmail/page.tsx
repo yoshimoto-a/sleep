f6b954a36a921462b164febdf048f89a130426d4
rs-akane <rs.akane.y@gmail.com>
2024-06-08 22:04:30 +0900

    ヘッダー追加

--- src/app/signup/sentEmail/page.tsx
+++ src/app/signup/sentEmail/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-
+import { Header } from "@/app/_components/header";
 import { Footer } from "@/app/dashboard/_components/footer";
 export default function EmailSent() {
   return (
     <>
+      <Header />
       <div className="absolute inset-0 flex items-center justify-center">
         <p>
           ご登録のメールアドレス宛にメールを送信しました。
