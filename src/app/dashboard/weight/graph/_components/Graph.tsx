118f5bc3885cd7904c7a617f737930a5efc8241d
rs-akane <rs.akane.y@gmail.com>
2024-07-05 23:17:03 +0900

    ラベル追加

--- src/app/dashboard/weight/graph/_components/Graph.tsx
+++ src/app/dashboard/weight/graph/_components/Graph.tsx
@@ -2,10 +2,10 @@ import {
   LineChart,
   XAxis,
   YAxis,
+  Label,
   Tooltip,
   Line,
   CartesianGrid,
-  Legend,
 } from "recharts";
 import { WeightGraph } from "@/app/_types/apiRequests/dashboard/weight/Index";
 interface Props {
@@ -15,18 +15,21 @@ export const Graph: React.FC<Props> = ({ data }) => {
   const yTicks = Array.from({ length: 13 }, (_, i) => i * 1000);
   const xTicks = Array.from({ length: 21 }, (_, i) => i);
   return (
-    <div className="w-full mt-10 flex justify-center">
+    <div className="w-full">
       <LineChart
-        width={480}
-        height={600}
+        width={380}
+        height={500}
         data={data}
-        margin={{ top: 30, right: 30, left: 0, bottom: 0 }}
+        margin={{ top: 30, right: 30, left: 15, bottom: 15 }}
       >
         <CartesianGrid strokeDasharray="1 1" />
-        <XAxis dataKey="monthAge" type="number" ticks={xTicks} interval={0} />
-        <YAxis ticks={yTicks} />
+        <XAxis dataKey="monthAge" type="number" ticks={xTicks} interval={0}>
+          <Label value="月齢(ヶ月)" position="bottom" offset={0} />
+        </XAxis>
+        <YAxis ticks={yTicks}>
+          <Label value="体重(g)" position="left" offset={0} angle={-90} />
+        </YAxis>
         <Tooltip />
-        <Legend />
         <Line type="monotone" dataKey="weight" stroke="#3B82F6" name="体重" />
       </LineChart>
     </div>
