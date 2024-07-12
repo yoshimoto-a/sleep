6b7c885904cd178aa1b314a3ea43dae399040df9
rs-akane <rs.akane.y@gmail.com>
2024-07-12 09:56:12 +0900

    新規追加

---
+++ src/app/api/dashboard/weekly_sleep_chart/_utils/splitDataByDate.ts
@@ -0,0 +1,39 @@
+import { SleepingSituation } from "@prisma/client";
+import { dayjs } from "../../../_utils/dayjs";
+export const splitDataByDate = (
+  data: SleepingSituation[],
+  dateRanges: { startOfDay: Date; endOfDay: Date }[]
+) => {
+  const result: SleepingSituation[][] = [];
+  dateRanges.forEach(range => {
+    const filteredData = data.filter(item => {
+      const bedTimeInRange =
+        item.bedTime &&
+        dayjs(item.bedTime).isBetween(
+          range.startOfDay,
+          range.endOfDay,
+          null,
+          "[]"
+        );
+      const sleepInRange =
+        item.sleep &&
+        dayjs(item.sleep).isBetween(
+          range.startOfDay,
+          range.endOfDay,
+          null,
+          "[]"
+        );
+      const wakeupInRange =
+        item.wakeup &&
+        dayjs(item.wakeup).isBetween(
+          range.startOfDay,
+          range.endOfDay,
+          null,
+          "[]"
+        );
+      return bedTimeInRange || sleepInRange || wakeupInRange;
+    });
+    result.push(filteredData);
+  });
+  return result;
+};
