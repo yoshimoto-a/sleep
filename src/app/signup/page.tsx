02c6e570dcd52ff3d403c72c4cab28ac79eb22ee
rs-akane <rs.akane.y@gmail.com>
2024-07-13 23:37:15 +0900

    カスタムフック切り出し

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,76 +1,25 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import React from "react";
-import { useState } from "react";
 import { ErrorMessage } from "../_components/ErrorMessage";
 import { Footer } from "../_components/footer";
 import { Form } from "../_components/form";
 import { Header } from "../_components/header";
 import { Input } from "../_components/input";
+import { useSignupForm } from "./_hooks/useSingnupForm";
 import { SubmitButton } from "@/app/_components/submitButton";
-import { supabase } from "@/utils/supabase";
-
 export default function Page() {
-  const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
-  const [emailErrorMessage, setEmailErrorMessage] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    passwordErrorMessage,
+    emailErrorMessage,
+    isSubmitting,
+    handleSubmit,
+  } = useSignupForm();
 
-  const valid = () => {
-    let isValid = true;
-    let emailError = "";
-    let passwordError = "";
-
-    if (!email) {
-      emailError = "メールアドレスは必須です。";
-      isValid = false;
-    } else if (!email.match(/.+@.+\..+/)) {
-      emailError = "メールアドレスの形式が正しくありません。";
-      isValid = false;
-    }
-    if (!password) {
-      passwordError = "パスワードは必須です。";
-      isValid = false;
-    } else if (password.length < 6) {
-      passwordError = "パスワードは6文字以上で入力してください。";
-      isValid = false;
-    }
-
-    setEmailErrorMessage(emailError);
-    setPasswordErrorMessage(passwordError);
-
-    return isValid;
-  };
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-    if (!valid()) {
-      setIsSubmitting(false);
-      return;
-    }
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/login`,
-      },
-    });
-    if (error) {
-      if (error.status === 422) {
-        alert("登録済のメールアドレスです");
-      } else {
-        alert("登録に失敗しました");
-      }
-    } else {
-      setEmail("");
-      setPassword("");
-      router.push("/signup/sentEmail/");
-    }
-    setIsSubmitting(false);
-  };
   return (
     <>
       <Header />
@@ -103,7 +52,6 @@ export default function Page() {
         </div>
         <SubmitButton disabled={isSubmitting}>送信</SubmitButton>
       </Form>
-
       <Footer />
     </>
   );
