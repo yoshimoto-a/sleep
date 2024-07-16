5a7f4de63b5f5a8c823f566a3906a0302709230a
rs-akane <rs.akane.y@gmail.com>
2024-07-16 22:18:48 +0900

    hook-form使用してバリデーション追加

--- src/app/login/_hooks/useLoginForm.ts
+++ src/app/login/_hooks/useLoginForm.ts
@@ -1,50 +1,55 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React from "react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { ApiResponse } from "../../_types/apiRequests/login/PostResponse";
 import { useApi } from "@/app/_hooks/useApi";
 import { PostRequests } from "@/app/_types/apiRequests/login/PostRequest";
 import { supabase } from "@/utils/supabase";
 
+type LoginFormInputs = {
+  email: string;
+  password: string;
+};
 export const useLoginForm = () => {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { post } = useApi();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = async (data: LoginFormInputs) => {
     setIsSubmitting(true);
     const toastId = toast.loading("ログイン処理中...");
     try {
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
+      const { data: signInData, error } =
+        await supabase.auth.signInWithPassword({
+          email: data.email,
+          password: data.password,
+        });
       if (error) {
         alert("ログインに失敗しました");
         toast.dismiss(toastId);
         setIsSubmitting(false);
         return;
       }
-      setEmail("");
-      setPassword("");
 
-      if (!data.session) throw new Error("session情報がありません");
+      if (!signInData.session) throw new Error("session情報がありません");
 
       //userテーブルの登録をする
       const prams: PostRequests = {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: data.session.access_token,
+          Authorization: signInData.session.access_token,
         },
         body: {
-          supabaseUserId: data.user.id,
+          supabaseUserId: signInData.user.id,
         },
       };
       const resp = await post<PostRequests, ApiResponse>("/api/login/", prams);
@@ -64,5 +69,10 @@ export const useLoginForm = () => {
       setIsSubmitting(false);
     }
   };
-  return { isSubmitting, handleSubmit, email, setEmail, password, setPassword };
+  return {
+    isSubmitting,
+    register,
+    handleSubmit: handleSubmit(onSubmit),
+    errors,
+  };
 };
