1fcf5140aee3bc8a3837eb4b7a50df5fa42b1c9f
rs-akane <rs.akane.y@gmail.com>
2024-07-18 08:58:44 +0900

    useFetch使用に書き替え

--- src/app/dashboard/_hooks/useGetBaby.tsx
+++ src/app/dashboard/_hooks/useGetBaby.tsx
@@ -1,29 +1,9 @@
-import useSWR from "swr";
-import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
+import { useFetch } from "@/app/_hooks/useFetch";
 import { IndexResponse } from "@/app/_types/apiRequests/dashboard/setting";
 
 export const useGetBaby = () => {
-  const { token, isLoding } = useSupabaseSession();
-  const shouldFetchData = !isLoding && token;
-  const fetcher = async () => {
-    if (!token) return;
-    const prams = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token,
-      },
-    };
-    const resp = await fetch(`/api/dashboard/setting`, prams);
-    const data: IndexResponse = await resp.json();
-    if (data.status !== 200) {
-      throw new Error("Failed to get data");
-    }
-    return data;
-  };
-  const { data, error, isLoading } = useSWR(
-    shouldFetchData ? `/api/dashboard/setting` : null,
-    fetcher
+  const { data, isLoading, error } = useFetch<IndexResponse>(
+    "/api/dashboard/setting"
   );
 
   return { isLoading, data, error };
