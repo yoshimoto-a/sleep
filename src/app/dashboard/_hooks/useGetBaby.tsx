7b9a69100b645dfbb7bb2875a0c9f948f92e40aa
rs-akane <rs.akane.y@gmail.com>
2024-07-18 13:50:34 +0900

    エンドポイントの修正

--- src/app/dashboard/_hooks/useGetBaby.tsx
+++ src/app/dashboard/_hooks/useGetBaby.tsx
@@ -2,9 +2,8 @@ import { useFetch } from "@/app/_hooks/useFetch";
 import { IndexResponse } from "@/app/_types/apiRequests/dashboard/setting";
 
 export const useGetBaby = () => {
-  const { data, isLoading, error } = useFetch<IndexResponse>(
-    "/api/dashboard/setting"
-  );
+  const { data, isLoading, error } =
+    useFetch<IndexResponse>("/dashboard/setting");
 
   return { isLoading, data, error };
 };
