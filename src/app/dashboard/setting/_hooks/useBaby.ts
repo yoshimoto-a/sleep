5c5bccde9172b8a521e5866354ec33f3347b8f1a
rs-akane <rs.akane.y@gmail.com>
2024-07-20 00:16:11 +0900

    リファクタリング

--- src/app/dashboard/setting/_hooks/useBaby.ts
+++ src/app/dashboard/setting/_hooks/useBaby.ts
@@ -3,29 +3,35 @@
 import { Gender } from "@prisma/client";
 import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useGetBaby } from "../../_hooks/useGetBaby";
 import { useGetWakeWindows } from "../../_hooks/useGetWakeWindows";
+import { useValidation } from "./useValidation";
 import { useApi } from "@/app/_hooks/useApi";
-import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
 import { ApiResponse } from "@/app/_types/apiRequests/apiResponse";
 import { UpdateRequests } from "@/app/_types/apiRequests/dashboard/setting/updateRequest";
 
 export const useBaby = () => {
-  const { token, isLoding } = useSupabaseSession();
   const { data, isLoading, error } = useGetBaby();
   const [babyName, setBabyName] = useState("");
   const [birthday, setBirthday] = useState("");
   const [expectedDateOfBirth, setExpectedDateOfBirth] = useState("");
   const [birthWeight, setBirthWeight] = useState("");
   const [gender, setGender] = useState<Gender | "">("");
+  const { errors, validate } = useValidation(
+    babyName,
+    birthday,
+    expectedDateOfBirth,
+    birthWeight,
+    gender
+  );
   const router = useRouter();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { error: wakeWindowsError } = useGetWakeWindows();
   const { put } = useApi();
-  //初期設定
+
   useEffect(() => {
+    if (isLoading) return;
     if (data && "data" in data) {
       const { data: babyData } = data;
       setBabyName(babyData.name);
@@ -36,12 +42,13 @@ export const useBaby = () => {
       );
       setGender(babyData.gender);
     }
-  }, [isLoding, data]);
+  }, [data, isLoading]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!validate()) return;
     setIsSubmitting(true);
-    if (token && gender) {
+    if (gender) {
       try {
         const body = {
           name: babyName,
@@ -50,21 +57,14 @@ export const useBaby = () => {
           birthWeight: parseInt(birthWeight),
           gender,
         };
-        const resp = await put<UpdateRequests, ApiResponse>(
-          "/api/dashboard/setting",
-          body
-        );
+        await put<UpdateRequests, ApiResponse>("/api/dashboard/setting", body);
         if (wakeWindowsError?.status === 204) {
           router.replace("/dashboard/wakeWindows");
           return;
+        } else {
+          router.replace("/dashboard/sleep/");
+          return;
         }
-
-        if (resp.status !== 200) {
-          throw new Error("更新に失敗しました。");
-        }
-
-        router.replace("/dashboard/sleep/");
-        return;
       } catch (e) {
         if (e instanceof Error) {
           alert(e.message);
@@ -88,5 +88,6 @@ export const useBaby = () => {
     setBirthWeight,
     gender,
     setGender,
+    errors,
   };
 };
