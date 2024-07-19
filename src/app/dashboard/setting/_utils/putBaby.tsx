b06e37416307e93869211276c7f15ff9dde3a754
rs-akane <rs.akane.y@gmail.com>
2024-07-19 13:57:40 +0900

    型に合わせて書き換え

--- src/app/dashboard/setting/_utils/putBaby.tsx
+++ src/app/dashboard/setting/_utils/putBaby.tsx
@@ -2,22 +2,20 @@ import { ApiResponse } from "@/app/_types/apiRequests/apiResponse";
 import { UpdateRequests } from "@/app/_types/apiRequests/dashboard/setting/updateRequest";
 import { Baby } from "@/app/_types/apiRequests/dashboard/setting/updateRequest";
 
-export const putBaby = async (token: string, body: Baby) => {
+export const putBaby = async (token: string, data: Baby) => {
   const prams: UpdateRequests = {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
     },
-    body: {
-      data: body,
-    },
+    data,
   };
 
   try {
     const resp = await fetch("/api/dashboard/setting", {
       ...prams,
-      body: JSON.stringify(prams.body),
+      body: JSON.stringify(prams.data),
     });
     if (resp.status !== 200) {
       throw new Error(`HTTP error. status: ${resp.status}`);
