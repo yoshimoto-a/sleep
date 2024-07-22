79998dfbb5561e9fe6db5449351b08fbfc0b11a6
rs-akane <rs.akane.y@gmail.com>
2024-07-23 00:05:32 +0900

    ファイル名変更

---
+++ src/app/dashboard/sleep/_component/ElapsedTime.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import { FormatDuration } from "../_utils/formatDuration";
+import { FindLatestResponse } from "@/app/api/dashboard/sleep/_utils/findLatest";
+
+interface PropsItem {
+  data: FindLatestResponse | undefined;
+}
+
+export const ElapsedTime: React.FC<PropsItem> = ({ data }) => {
+  const [action, setAction] = useState<string>("");
+  const [elapsedTime, setElapsedTime] = useState<string | null>(null);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 60000);
+
+    return () => clearInterval(interval);
+  }, []);
+  useEffect(() => {
+    if (!data) return;
+    if (!data) {
+      setAction("");
+      setElapsedTime("登録なし");
+      return;
+    } else {
+      switch (data.action) {
+        case "寝かしつけ開始":
+          setAction("入眠所要時間");
+          if (data.record.bedTime)
+            setElapsedTime(
+              FormatDuration(data.record.bedTime, currentTime, "MinutesOnly")
+            );
+          break;
+        case "寝た":
+          setAction("睡眠時間");
+          if (data.record.sleep)
+            setElapsedTime(
+              FormatDuration(data.record.sleep, currentTime, "HourAndMinutes")
+            );
+          break;
+        case "起きた":
+          setAction("活動時間");
+          if (data.record.wakeup)
+            setElapsedTime(
+              FormatDuration(data.record.wakeup, currentTime, "HourAndMinutes")
+            );
+          break;
+      }
+      return;
+    }
+  }, [data, data?.action, data?.record, currentTime]);
+
+  return (
+    <div className="rounded-md bg-white w-40 pt-2 text-center">
+      <span className="text-sm">{action}</span>
+      <div className="flex items-center justify-center">
+        <span className="text-3xl pb-3">{elapsedTime}</span>
+      </div>
+    </div>
+  );
+};
