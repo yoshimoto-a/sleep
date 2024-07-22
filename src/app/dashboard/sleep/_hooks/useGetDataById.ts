3b009f90f34d9d1e90cd336fdb85a3abc7b88cc7
rs-akane <rs.akane.y@gmail.com>
2024-07-23 00:00:53 +0900

    useFetch使用

--- src/app/dashboard/sleep/_hooks/useGetDataById.ts
+++ src/app/dashboard/sleep/_hooks/useGetDataById.ts
@@ -1,29 +1,10 @@
 "use client";
-import useSWR from "swr";
-import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
+import { useFetch } from "@/app/_hooks/useFetch";
 import { IndexResponse } from "@/app/_types/apiRequests/dashboard/sleepDetail/IndexResponse";
 
 export const useGetDataById = (id: number) => {
-  const { token, isLoding } = useSupabaseSession();
-  const shouldFetchData = !isLoding && token;
-  const fetcher = async () => {
-    if (!token) return;
-    const resp = await fetch(`/api/dashboard/sleepDetail?id=${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token,
-      },
-    });
-    const data: IndexResponse = await resp.json();
-    if (data.status !== 200) {
-      throw new Error("error in fetcher");
-    }
-    return data;
-  };
-  const { data, error, isLoading, mutate } = useSWR(
-    shouldFetchData ? `/api/dashboard/sleepDetail?id=${id}` : null,
-    fetcher
+  const { isLoading, data, error, mutate } = useFetch<IndexResponse>(
+    `dashboard/sleepDetail?id=${id}`
   );
 
   return { isLoading, data, error, mutate };
--- src/app/dashboard/sleep/_hooks/useGetNextSleepTime.ts
+++ src/app/dashboard/sleep/_hooks/useGetNextSleepTime.ts
@@ -1,35 +1,9 @@
-import useSWR from "swr";
-import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
+import { useFetch } from "@/app/_hooks/useFetch";
 import { IndexResponse } from "@/app/_types/apiRequests/dashboard/nextSleepTime";
 
 export const useGetNextSleepTime = () => {
-  const { token, isLoding } = useSupabaseSession();
-  const shouldFetchData = !isLoding && token;
-  const fetcher = async () => {
-    if (!token) return;
-    const prams = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token,
-      },
-    };
-    const resp = await fetch("/api/dashboard/nextSleepTime", prams);
-    const data: IndexResponse = await resp.json();
-
-    if (
-      data.status === 200 ||
-      data.error === "no wakeWindowsData" ||
-      data.error === "no sleepingSituationData"
-    ) {
-      return data;
-    } else {
-      throw new Error("error in fetcher");
-    }
-  };
-  const { data, error, isLoading, mutate } = useSWR(
-    shouldFetchData ? "/api/dashboard/nextSleepTime" : null,
-    fetcher
+  const { data, error, isLoading, mutate } = useFetch<IndexResponse>(
+    "dashboard/nextSleepTime"
   );
 
   return { isLoading, data, error, mutate };
