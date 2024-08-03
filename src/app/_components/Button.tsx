3d28a904d858bfcbe76a770fd4b30faaf430e852
rs-akane <rs.akane.y@gmail.com>
2024-08-03 12:59:38 +0900

    ComponentPropsWithRef、ref対応に修正

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, ComponentPropsWithRef, forwardRef } from "react";
 
 type Variant =
   | "outlined"
@@ -7,46 +7,39 @@ type Variant =
   | "contained-blu500"
   | "none";
 
-type Type = "button" | "submit";
-
-interface Props {
-  variant: Variant;
-  type: Type;
-  onClick?: () => void;
-  children: ReactNode;
-  disabled?: boolean;
+interface Props extends ComponentPropsWithRef<"button"> {
+  variant?: Variant;
+  children?: ReactNode;
 }
 
-export const Button: React.FC<Props> = ({
-  variant,
-  type,
-  onClick,
-  children,
-  disabled,
-}) => {
-  const className = () => {
-    switch (variant) {
-      case "outlined":
-        return "border-solid border-2 border-slate-600";
-      case "contained-blu":
-        return "bg-custom-blue";
-      case "contained-gry":
-        return "bg-gray-300";
-      case "contained-blu500":
-        return "bg-blue-500 text-white";
-      case "none":
-        return "";
-    }
-  };
+export const Button = forwardRef<HTMLButtonElement, Props>(
+  ({ variant, children, ...props }, ref) => {
+    const className = () => {
+      switch (variant) {
+        case "outlined":
+          return "border-solid border-2 border-slate-600";
+        case "contained-blu":
+          return "bg-custom-blue";
+        case "contained-gry":
+          return "bg-gray-300";
+        case "contained-blu500":
+          return "bg-blue-500 text-white";
+        case "none":
+          return "";
+        default:
+          return "";
+      }
+    };
+    return (
+      <button
+        ref={ref}
+        {...props}
+        className={`w-full h-full rounded-full ${className()}`}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
-  return (
-    <button
-      type={type}
-      onClick={onClick}
-      className={`w-full h-full rounded-full ${className()}`}
-      disabled={disabled}
-    >
-      {children}
-    </button>
-  );
-};
+Button.displayName = "Button";
