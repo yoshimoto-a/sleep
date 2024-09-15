37dbb561cef0df9c6d7b2e6b29995838d51a6cd6
rs-akane <rs.akane.y@gmail.com>
2024-09-15 22:41:29 +0900

    URLパラメータの状態管理追加

--- src/app/dashboard/sleep/_hooks/useSleepDashboard.ts
+++ src/app/dashboard/sleep/_hooks/useSleepDashboard.ts
@@ -1,17 +1,40 @@
 "use client";
 
-import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
 import { dayjs } from "../../../../utils/dayjs";
 import { useGetData } from "./../_hooks/useGetData";
-
 export const useSleepDashBoard = () => {
-  const [date, setDate] = useState(new Date());
-  const { isLoading, data, error, mutate } = useGetData(date);
+  const searchParams = useSearchParams();
+  const currentDate = searchParams.get("date")
+    ? new Date(searchParams.get("date") as string)
+    : new Date();
+  const [selectedDate, setSelectedDate] = useState(currentDate);
+  const { isLoading, data, error, mutate } = useGetData(selectedDate);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    params.set("date", selectedDate.toISOString());
+    const newUrl = `${window.location.pathname}?${params.toString()}`;
+    window.history.replaceState({}, "", newUrl);
+  }, [selectedDate]);
+
   const handlePrev = () => {
-    setDate(dayjs(date).add(-1, "d").toDate());
+    const newDate = dayjs(selectedDate).add(-1, "d").toDate();
+    setSelectedDate(newDate);
   };
   const handleNext = () => {
-    setDate(dayjs(date).add(1, "d").toDate());
+    const newDate = dayjs(selectedDate).add(1, "d").toDate();
+    setSelectedDate(newDate);
   };
-  return { isLoading, data, error, mutate, handleNext, handlePrev, date };
+  return {
+    isLoading,
+    data,
+    error,
+    mutate,
+    handleNext,
+    handlePrev,
+    date: selectedDate,
+    setSelectedDate,
+  };
 };
